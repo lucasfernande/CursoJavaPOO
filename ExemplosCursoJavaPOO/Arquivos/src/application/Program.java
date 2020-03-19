865f147621835adde02332e15989f21b1954c28f
lucas1914 <lucas.fernandes6192@outlook.com>
2020-03-19 15:40:43 -0300

    FileWriter e BufferedWriter

--- ExemplosCursoJavaPOO/Arquivos/src/application/Program.java
+++ ExemplosCursoJavaPOO/Arquivos/src/application/Program.java
@@ -1,26 +1,27 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		String path = "C:\\Users\\Familias\\Documents\\ws-eclipse\\teste.txt";
-
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-
-			String line = br.readLine();
-
-			while (line != null) {
-				System.out.println(line);
-				line = br.readLine();
+		String[] lines = new String[] {"Good morning", "Good afternoon", "Good night"};
+		
+		String path = "C:\\Users\\Familias\\Documents\\ws-eclipse\\test.txt";
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
 			}
 		}
 		catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+
 	}
 }
