fc359e99e105797f2dc0b9ff03be2e348ef2d9a9
lucas1914 <lucas.fernandes6192@outlook.com>
2020-03-19 15:56:48 -0300

    Informações do caminho do arquivo

--- ExemplosCursoJavaPOO/Arquivos/src/application/Program.java
+++ ExemplosCursoJavaPOO/Arquivos/src/application/Program.java
@@ -14,23 +14,9 @@ public class Program {
         
         File path = new File(strPath);
         
-        File[] folders = path.listFiles(File::isDirectory); // esse referenceMethods lista apenas quem é diretório, ou seja, pasta
-        
-        System.out.println("FOLDERS: ");
-        for(File folder : folders) {
-        	System.out.println(folder);
-        }
-        
-        File[] files = path.listFiles(File::isFile); // e esse lista somente arquivos
-        System.out.println();
-        System.out.println("FILES: ");
-        for (File file : files) {
-        	System.out.println(file);
-        }
-        
-        boolean success = new File(strPath + "\\subDir").mkdir();
-        System.out.println("Directory created succesfully: " + success);
-        
+        System.out.println("getName: " + path.getName());
+        System.out.println("getParent: " + path.getParent());
+        System.out.println("getPath: " + path.getPath());
         sc.close();
 
 	}
